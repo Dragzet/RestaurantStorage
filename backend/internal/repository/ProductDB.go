6dcc094e7d92ac5736e9fb7fd509d615368d12d6
Senyasha <arharit27@gmail.com>
2024-11-21 19:23:29 +0300

    fix connections closing

--- backend/internal/repository/ProductDB.go
+++ backend/internal/repository/ProductDB.go
@@ -4,6 +4,7 @@ import (
 	"RestaurantStorage/internal/models"
 	"context"
 	"fmt"
+	"time"
 )
 
 func (d *DB) AddProduct(ctx context.Context, product models.ProductModel) error {
@@ -12,12 +13,14 @@ func (d *DB) AddProduct(ctx context.Context, product models.ProductModel) error 
 			(name, amount, image)
 		VALUES
 			($1, $2, $3)`
-	err := d.findProduct(ctx, product.Name)
+	cont, cancel := context.WithTimeout(ctx, time.Second*5)
+	defer cancel()
+	err := d.findProduct(cont, product.Name)
 	if err == nil {
 		return fmt.Errorf("ProductDB: AddProduct: Product already exists")
 	}
 
-	_, err = d.Client.Exec(ctx, stmt, product.Name, product.Amount, product.Image)
+	_, err = d.Client.Exec(cont, stmt, product.Name, product.Amount, product.Image)
 	if err != nil {
 		return fmt.Errorf("Error in ProductDB: AddProduct: %s", err.Error())
 	}
@@ -27,14 +30,15 @@ func (d *DB) AddProduct(ctx context.Context, product models.ProductModel) error 
 
 func (d *DB) GetProduct(ctx context.Context, name string) (*models.ProductModel, error) {
 	stmt := `SELECT * FROM products WHERE name = $1`
-
-	err := d.findProduct(ctx, name)
+	cont, cancel := context.WithTimeout(ctx, time.Second*5)
+	defer cancel()
+	err := d.findProduct(cont, name)
 	if err != nil {
 		return nil, err
 	}
 
 	var productItem models.ProductModel
-	err = d.Client.QueryRow(ctx, stmt, name).Scan(&productItem.ID, &productItem.Name, &productItem.Amount, &productItem.Image)
+	err = d.Client.QueryRow(cont, stmt, name).Scan(&productItem.ID, &productItem.Name, &productItem.Amount, &productItem.Image)
 	if err != nil {
 		return nil, fmt.Errorf("Error in ProductDB: GetProduct: %s", err.Error())
 	}
@@ -43,11 +47,14 @@ func (d *DB) GetProduct(ctx context.Context, name string) (*models.ProductModel,
 
 func (d *DB) ChangeProduct(ctx context.Context, name string, amount int) error {
 	stmt := `UPDATE products SET amount = $2 WHERE name = $1`
-	err := d.findProduct(ctx, name)
+	cont, cancel := context.WithTimeout(ctx, time.Second*5)
+	defer cancel()
+	err := d.findProduct(cont, name)
 	if err != nil {
 		return err
 	}
-	_, err = d.Client.Query(ctx, stmt, name, amount)
+	rows, err := d.Client.Query(cont, stmt, name, amount)
+	defer rows.Close()
 	if err != nil {
 		return fmt.Errorf("Error in ProductDB: ChangeProduct: %s", err.Error())
 	}
@@ -58,7 +65,9 @@ func (d *DB) findProduct(ctx context.Context, name string) error {
 	errorStatement := "ProductDB: findProduct"
 	stmt := `SELECT * FROM products WHERE name = $1`
 	var productItem models.ProductModel
-	err := d.Client.QueryRow(ctx, stmt, name).Scan(&productItem.ID, &productItem.Name, &productItem.Amount, &productItem.Image)
+	cont, cancel := context.WithTimeout(ctx, time.Second*5)
+	defer cancel()
+	err := d.Client.QueryRow(cont, stmt, name).Scan(&productItem.ID, &productItem.Name, &productItem.Amount, &productItem.Image)
 	if err != nil {
 		fmt.Println(err.Error())
 		return fmt.Errorf("Error in %s: %s", errorStatement, err.Error())
@@ -68,12 +77,14 @@ func (d *DB) findProduct(ctx context.Context, name string) error {
 
 func (d *DB) DeleteProduct(ctx context.Context, name string) error {
 	stmt := `DELETE FROM products WHERE name = $1`
-	err := d.findProduct(ctx, name)
+	cont, cancel := context.WithTimeout(ctx, time.Second*5)
+	defer cancel()
+	err := d.findProduct(cont, name)
 	if err != nil {
 		return err
 	}
 
-	_, err = d.Client.Query(ctx, stmt, name)
+	_, err = d.Client.Query(cont, stmt, name)
 	if err != nil {
 		return fmt.Errorf("Error in ProductDB: DeleteProduct: %s", err.Error())
 	}
@@ -82,7 +93,9 @@ func (d *DB) DeleteProduct(ctx context.Context, name string) error {
 
 func (d *DB) GetProductList(ctx context.Context) ([]*models.ProductModel, error) {
 	stmt := `SELECT * FROM products`
-	rows, err := d.Client.Query(ctx, stmt)
+	cont, cancel := context.WithTimeout(ctx, time.Second*5)
+	defer cancel()
+	rows, err := d.Client.Query(cont, stmt)
 	if err != nil {
 		return nil, fmt.Errorf("Error in ProductDB: GetProductList: %s", err.Error())
 	}
